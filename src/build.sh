1c07b1378c34404f30ab500ce2811659a7fed3d4
duandingbo@jinghang.ai <duandingbo@jinghang.ai>
2019-02-14 17:05:55 +0800

    update and test

--- src/build.sh
+++ src/build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 #export GOPATH=$(realpath ../../)
+exe_name="speech_eval"
+config_name="config.json"
+lib_name="lib"
 export GOPATH="$(go env GOPATH):$(pwd)/vendor"
 _version="0.1" 
 _build_time="$(date +%Y%m%d-%H:%M:%S)" 
@@ -13,6 +16,7 @@ fi
 #CGO_LDFLAGS="-Wl,-rpath=./lib "  go build -a -v  -ldflags "-X main._VERSION_=$ver -X main._TYPE_=$_type"   -o speech_eval
 CGO_LDFLAGS="-Wl,-rpath=./lib "  go build -a  -v  -ldflags "-X main._VERSION_=$_version -X main._TYPE_=$_type  -X main._BUILD_TIME_=$_build_time "   -o speech_eval
 
+cp -r $exe_name $lib_name $config_name ../bin
 
 echo $GOPATH
 
